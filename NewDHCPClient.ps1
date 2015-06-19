a09f49cb50aee4bb3f0c95caf640839df13fc827
scott <macca728@hotmail.com>
2015-06-19 14:11:01 +1000

    init commit

---
+++ NewDHCPClient.ps1
@@ -0,0 +1,49 @@
+#v1.0
+#Written by nojeffrey(https://github.com/nojeffrey)
+#Email if new DHCP client is found on Windows DHCP Server, tested on 2008r2
+#Modify these 4 variables to suit:
+
+$DHCPServer = "yourDHCPserver"
+$SMTPServer = "yourMAILserver"
+$from = "DHCP@yourcompany.com"
+$to = "you@yourcompany.com"
+
+
+#Test if C:\DHCP directory exists, if not; create the dir and run for the first time, save output to C:\DHCP\DHCPList.txt
+if((Test-Path C:\DHCP) -eq 0){
+    New-Item -ItemType Directory -Path C:\DHCP
+    Get-DhcpServerv4Scope -ComputerName $DHCPServer | Get-DhcpServerv4Lease -ComputerName $DHCPServer | Select-Object -ExpandProperty Hostname | Out-File "C:\DHCP\DHCPList.txt"
+    }
+
+
+$old = Get-Content "C:\DHCP\DHCPList.txt" 
+$new = Get-DhcpServerv4Scope -ComputerName $DHCPServer | Get-DhcpServerv4Lease -ComputerName $DHCPServer | Select-Object -ExpandProperty Hostname 
+
+#Compare difference
+$newDHCPtoWrite = diff -ReferenceObject $old -DifferenceObject $new
+$ListToEmail = $null
+$count = 0
+
+
+ForEach ( $x in $newDHCPtoWrite){
+    #I only care if new DHCP Client(=>), ignore clients that drop off DHCP(<=)
+    if($x.SideIndicator -eq "=>") {
+        $count += 1
+        $ListToEmail += "</br>" + $x.InputObject
+        #Append newly found Clients to DHCPList.txt
+        $x.InputObject | Out-File "C:\DHCP\DHCPList.txt" -Append
+        }
+}
+
+#If no new DHCP clients, exit script now
+if($count -eq 0){
+    Exit
+    }
+
+#Send email
+$subject = "DHCP: New Client(s)"
+$body = "There is " + $count + " never before seen DHCP Clients: "+ $ListToEmail 
+$mailer = new-object Net.Mail.SMTPclient($SMTPServer)
+$msg = new-object Net.Mail.MailMessage($from,$to,$subject,$body)
+$msg.IsBodyHTML = $true
+$mailer.send($msg)
---
+++ README.md
@@ -0,0 +1,5 @@
+## New-DHCP-Client-Powershell
+- Sends out an email when a new DHCP client is seen for the first time.
+- The first time running this it will create the directory structure C:\DHCP if this doesn't exist, and run for the first time creating a benchmark.
+- For the first few days it should be noisey as people come and go from wifi.
+- Modify the first 4 variables to suit your environment.
