37fe6f376f91281d1897f04f04bc8a5c16bb5d04
scott <macca728@hotmail.com>
2015-06-24 11:29:40 +1000

    added some testing for file/directories & general clean up

--- NewDHCPClient.ps1
+++ NewDHCPClient.ps1
@@ -1,6 +1,7 @@
-#v1.01
+#v1.02
 #Written by nojeffrey(https://github.com/nojeffrey)
-#Email if new DHCP client is found on Windows DHCP Server, tested on 2008r2
+#Email if new DHCP client is found on Windows DHCP Server, tested against 2008r2 DHCP server,
+#Run this from either a Windows 10, or Server2012 box, Get-DhcpServerv4Scope an Get-DhcpServerv4Lease functions are only available in I think Powershell 5.x
 #Modify these 4 variables to suit:
 
 $DHCPServer = "yourDHCPserver"
@@ -8,16 +9,20 @@ $SMTPServer = "yourMAILserver"
 $from = "DHCP@yourcompany.com"
 $to = "you@yourcompany.com"
 
-
 #Test if C:\DHCP directory exists, if not; create the dir and run for the first time, save output to C:\DHCP\DHCPList.txt
 if((Test-Path C:\DHCP) -eq 0){
     New-Item -ItemType Directory -Path C:\DHCP
-    Get-DhcpServerv4Scope -ComputerName $DHCPServer | Get-DhcpServerv4Lease -ComputerName $DHCPServer | Select-Object -ExpandProperty Hostname | Sort-Object -Unique | Out-File "C:\DHCP\DHCPList.txt"
-    }
+    Get-DhcpServerv4Scope -ComputerName $DHCPServer | Get-DhcpServerv4Lease -ComputerName $DHCPServer | Select-Object -ExpandProperty Hostname |Sort-Object -Unique | Out-File "C:\DHCP\DHCPList.txt"
+}
+
+#Test if DHCPList.txt exists, if not run for first time(needed if only the directory exists, and not the file)
+if((Test-Path C:\DHCP\DHCHList.txt) -eq 0){
+    Get-DhcpServerv4Scope -ComputerName $DHCPServer | Get-DhcpServerv4Lease -ComputerName $DHCPServer | Select-Object -ExpandProperty Hostname |Sort-Object -Unique | Out-File "C:\DHCP\DHCPList.txt"
+}
 
 
 $old = Get-Content "C:\DHCP\DHCPList.txt" | Sort-Object -Unique
-$new = Get-DhcpServerv4Scope -ComputerName $DHCPServer | Get-DhcpServerv4Lease -ComputerName $DHCPServer | Select-Object -ExpandProperty Hostname | Sort-Object -Unique
+$new = Get-DhcpServerv4Scope -ComputerName newdc01 | Get-DhcpServerv4Lease -ComputerName $DHCPServer | Select-Object -ExpandProperty Hostname | Sort-Object -Unique
 
 #Compare difference
 $newDHCPtoWrite = diff -ReferenceObject $old -DifferenceObject $new
@@ -25,25 +30,27 @@ $ListToEmail = $null
 $count = 0
 
 
-ForEach ( $x in $newDHCPtoWrite){
+foreach ( $x in $newDHCPtoWrite){
     #I only care if new DHCP Client(=>), ignore clients that drop off DHCP(<=)
     if($x.SideIndicator -eq "=>") {
         $count += 1
         $ListToEmail += "</br>" + $x.InputObject
         #Append newly found Clients to DHCPList.txt
         $x.InputObject | Out-File "C:\DHCP\DHCPList.txt" -Append
-        }
+    }
 }
 
-#If no new DHCP clients, exit script now
-if($count -eq 0){
-    Exit
-    }
-
+#Send email if count > 0
+if($count){
 #Send email
-$subject = "DHCP: New Client(s)"
-$body = "There is " + $count + " never before seen DHCP Clients: "+ $ListToEmail 
-$mailer = new-object Net.Mail.SMTPclient($SMTPServer)
-$msg = new-object Net.Mail.MailMessage($from,$to,$subject,$body)
-$msg.IsBodyHTML = $true
-$mailer.send($msg)
+    $subject = "DHCP: New Client(s)"
+    $body = "There is " + $count + " never before seen DHCP Client(s): "+ $ListToEmail
+    $mailer = new-object Net.Mail.SMTPclient($SMTPServer)
+    $msg = new-object Net.Mail.MailMessage($from,$to,$subject,$body)
+    $msg.IsBodyHTML = $true
+    $mailer.send($msg)
+}
+
+else{
+    break
+}
